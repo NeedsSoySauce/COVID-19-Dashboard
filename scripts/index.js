6bd688c1d1e2153c3083cef22d73ef20c5f320f1
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-08-27 03:49:21 +1200

    Added value labels

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,8 @@ const DEFAULT_OPTIONS = {
         bottom: 0,
         left: 0,
         right: 0
-    }
+    },
+    valueLabelOffset: 0
 };
 
 async function getSummaryData() {
@@ -32,7 +33,7 @@ async function getSummaryData() {
 }
 
 function drawBarChart(data, query, options) {
-    let { responsive, width, height, margin } = options;
+    let { width, height, margin, valueLabelOffset } = options;
     let chartWidth = width - margin.left - margin.right;
     let chartHeight = height - margin.top - margin.bottom;
 
@@ -52,11 +53,13 @@ function drawBarChart(data, query, options) {
 
     const yScale = d3.scaleBand().domain(keys).range([chartHeight, 0]).padding(0.1);
 
-    let xAxis = d3.axisTop(xScale).tickFormat(d3.format('.2s'));
+    let valueFormatter = d3.format('.2s');
+    let xAxis = d3.axisTop(xScale).tickFormat(valueFormatter);
     let yAxis = d3.axisLeft(yScale);
+    let barWidth = yScale.bandwidth();
 
     if (width < BREAKPOINTS.sm) {
-        xAxis.ticks(5)
+        xAxis.ticks(5);
     }
 
     // Draw axes representing the scales we created above
@@ -74,12 +77,34 @@ function drawBarChart(data, query, options) {
         .attr('width', (s) => xScale(s.value))
         .attr('height', yScale.bandwidth());
 
-    // Add vertical grid lineschart.append('g')
+    // Add vertical grid lines
     chart
         .append('g')
         .attr('class', 'grid')
         .attr('transform', `translate(0, ${height})`)
         .call(d3.axisBottom().scale(xScale).tickSize(-height, 0, 0).tickFormat(''));
+
+    // Add value labels
+    chart
+        .append('g')
+        .selectAll()
+        .data(data)
+        .enter()
+        .append('text')
+        .attr('class', 'value-label')
+        .text((s) => valueFormatter(s.value))
+        .attr('y', (s) => yScale(s.key) + barWidth / 2)
+        .attr('x', function (s) {
+            // This must be done after this node's content is set (via .text)
+            let offset = this.getComputedTextLength() / 2 + valueLabelOffset;
+            let xpos = xScale(s.value);
+            let x = xpos + offset;
+            if (x > chartWidth) {
+                this.classList.add('white');
+                return xpos - offset;
+            }
+            return x;
+        });
 }
 
 function barchart(data, query, options = {}) {
@@ -106,6 +131,7 @@ function barchart(data, query, options = {}) {
             height = bb.height || height;
 
             let currentOptions = {
+                ...options,
                 responsive,
                 width,
                 height,
@@ -115,6 +141,7 @@ function barchart(data, query, options = {}) {
             drawBarChart(data, query, currentOptions);
         } else {
             let currentOptions = {
+                ...options,
                 responsive,
                 width,
                 height,
@@ -153,7 +180,8 @@ window.addEventListener('DOMContentLoaded', async () => {
             bottom: 20,
             left: 30,
             right: 20
-        }
+        },
+        valueLabelOffset: 3
     };
 
     barchart(data, '#summary-chart', options);
